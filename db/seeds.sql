92ebb34190c2953cc861e7cb0d1fa74710fb205a
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-25 10:26:02 -0500

    updating seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,9 @@
 INSERT INTO department (name)
 VALUES
-('ENGINEERING'),
-('FINANCE'),
-('SALES'),
-('LEGAL');
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-('Sales Lead', 100000, 1),
-('Salesperson', 80000, 2),
-('Lead Engineer', 150000, 3),
-('Software Engineer', 120000, 4),
-('Account Manager', 160000, 5),
-('Accountant', 125000, 6),
-('Legal Team Lead', 250000, 7),
-('Lawyer', 190000, 8);
 
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES
+)
