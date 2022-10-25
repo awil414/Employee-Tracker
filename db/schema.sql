e97894580d2c7b3cce15c4ee90bc1a27e1848318
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-25 13:22:39 -0500

    troubleshooting schema tables and adding NOT NULL'

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ USE employee_db;
 
 -- Department table--
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    dept_name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    names VARCHAR(30) NOT NULL
 );
 
 -- Roles table --
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
@@ -21,12 +21,12 @@ CREATE TABLE roles (
 
 -- Employee table --
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (roles_id) REFERENCES roles(id),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id),
 );
 
