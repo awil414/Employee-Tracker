ed688c8ed5f0d0371cfc1c34271ce92b29cb2950
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-24 21:50:03 -0500

    adding schema.sql db, installed packages, and adding app.js

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+-- Creating the databases --
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- Department table--
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+-- Roles table --
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Employee table --
+CREATE TABLE employee (
+    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (roles_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+);
+
